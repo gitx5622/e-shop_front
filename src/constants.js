5b9969a681d2251cbff4c9d4f329a5c61581ed56
Anthony Gitau <gitau.ag@gmail.com>
2020-01-09 00:06:16 +0300

    http fix2

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ let API_ROUTE;
 
 process.env.NODE_ENV === 'development'
     ? API_ROUTE = 'http://127.0.0.1:9000'
-    : API_ROUTE = 'https://jobhub.co.ke:9000';
+    : API_ROUTE = 'http://167.71.137.103:9000';
 
 export default API_ROUTE
 
