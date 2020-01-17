829a9091490e5d07d22d50254c3ea4f18bdbf92b
gitx5622 <ggitau5622@gmail.com>
2020-01-17 20:18:20 +0300

    subscribe implementation

--- src/constants.js
+++ src/constants.js
@@ -4,6 +4,6 @@ let API_ROUTE;
 if (process.env.NODE_ENV === 'production') {
     API_ROUTE = 'https://admin.likee.co.ke'
 }else {
-    API_ROUTE = 'http://127.0.0.1:9000';
+    API_ROUTE = 'http://127.0.0.1:8000';
 }
 export default API_ROUTE;
